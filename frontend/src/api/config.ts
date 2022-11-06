aac1504de72b0b65a74f854456c5287613441be1
Ben Y <ben@Bens-MacBook-Pro.local>
2022-11-06 01:49:33 -0800

    Hooked up mintParent()

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -18,16 +18,16 @@ function getStagConfig() {
       url: backendURL ?? "http://thisabled-backend:3000"
     },
     addresses: {
-      parent: '0x4eB871943Af8F302536b78D7Ed3167f13bB931FA',
-      child1: '0x0b846F3c5505236d5Cc8a4fD15ee5cF174db2987',
-      child2: '0x03D08D67a32659816e4F8c4c7813251789bf256FZ',
-      storefrontAddress: '0xa57079afC7a1E9A38c6368d9789bE007Da9101b6'
+      parent: '0x1892b2bE1BA9d91360777E46748a39Fd5Bd4822C',
+      child1: '0x124ca2A83819819bEc250A7bED9D3b847FB2936B',
+      child2: '0x051d1A22E58b62104aD112Da585EcFafD5c48D14',
+      storefrontAddress: '0xc24c5028663850bfDdae07e246C731D5c4Ae857A'
     },
     contractParams: {
       chainId: `0x${chainNum.toString(16)}`,
       chainName: "Mumbai Testnet",
       rpcUrls: [process.env.MUMBAI_RPC],
-      blockExplorerUrls: ["https://polygonscan.com/"],
+      blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
       nativeCurrency: {
         name: "Mumbai Matic",
         symbol: "MATIC",
